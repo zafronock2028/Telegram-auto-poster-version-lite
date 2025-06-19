5f1905aae76e6ebc04c530a85961ec748eeed47e
zafronock2028 <zafronockbitfratsg@gmail.com>
2025-06-19 19:56:18 -0400

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'una_clave_secreta_muy_segura'
-    REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
-    SESSION_TTL = 300  # 5 minutos para códigos de verificación
