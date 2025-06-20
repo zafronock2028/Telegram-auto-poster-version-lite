73cb97be9863fbe1ad4042009f379848311cc9f4
zafronock2028 <zafronockbitfratsg@gmail.com>
2025-06-20 00:12:43 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,8 @@ pip install -r requirements.txt
 # Mensaje de verificación
 echo "¡Build completado exitosamente!"
 
+# Configurar variable de entorno para Render
+export RENDER=true
+
 # Iniciar la aplicación
 exec gunicorn --bind 0.0.0.0:$PORT --worker-class gunicorn.workers.ggevent.GeventWorker --workers 4 fameviz_panel:app
