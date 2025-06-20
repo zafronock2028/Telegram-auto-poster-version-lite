79837e9f98abd999ca785236ce131e4a65358010
zafronock2028 <zafronockbitfratsg@gmail.com>
2025-06-20 00:48:06 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ pip install -r requirements.txt
 # Mensaje de verificación
 echo "¡Build completado!"
 
-# Iniciar la aplicación
-exec gunicorn --bind 0.0.0.0:$PORT --worker-class gunicorn.workers.ggevent.GeventWorker --workers 1 fameviz_panel:app
+# Iniciar la aplicación con 1 solo worker para evitar problemas
+exec gunicorn --bind 0.0.0.0:$PORT --workers 1 fameviz_panel:app
