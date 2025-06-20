573b0c5b4555e669eeb8036cbebade1af253258d
zafronock2028 <zafronockbitfratsg@gmail.com>
2025-06-19 20:01:24 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 # Instalar dependencias
 pip install -r requirements.txt
 
+# No necesitamos configuración adicional de Redis
+echo "¡Sistema listo para iniciar!"
+
 # Iniciar la aplicación
 exec gunicorn --bind 0.0.0.0:$PORT fameviz_panel:app
